86768729b00e5d4322fe41f58e5fe46fa40884d0
Amit Dabade <31438237+amitdabade@users.noreply.github.com>
2019-09-06 10:09:52 +0530

    logic for no results added

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -14,32 +14,45 @@ export class SearchComponent implements OnInit {
   err:boolean = false;
   default_results:boolean = false;
   loader:boolean = false;
+  noResults:boolean = false;
   
   constructor(private api: ApiService) {
     this.api=api;
   }
   
   ngOnInit(){
-
   }
-  
+
+  // Enter key detection for searching
+  submitResult(e){
+    if (e.key === "Enter") {
+      document.getElementById('search_btn').click();
+    }
+  }
+
+  // Search GIF by user query
   searchResult(e,data){
     if(!data || data.search.trim()==""){
       this.err = true;
       this.default_results = false;
+      this.noResults = false;
       e.preventDefault();
     }else{
+      this.noResults = false;
       this.loader = true;
       this.err = false;
       this.default_results = true;
-      console.log(data.search);
+      // API call to giphy to get gifs
       this.api.getGiphy(data.search).subscribe((res)=>{
           this.res = res;
-          console.log(this.res.data);
           this.results=this.res.data;
           this.loader = false;
+          if(this.results.length==0){
+            this.noResults = true;
+          }else{
+            this.noResults = false;
+          }
       });
     }
   }
-
 }
